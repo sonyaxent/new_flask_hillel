f1537099593d70a2ea1c4a1bd9e905dba208df44
sonyaxent <sonyaxent@gmail.com>
2022-07-02 21:26:05 +0200

    Это домашняя работа по вступительному уроку flask.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
 import random, string
-
+import pandas as pd
+import csv
+import numpy
 
 from flask import Flask
 
@@ -22,7 +24,18 @@ def generate_password():
         password = password + random.choice(characters)
     return f'Password generated: {password}'
 
+@app.route("/average_parameters")
 def get_average_parameters():
+    col_names = [
+        'Id',
+        'Height',
+        'Weight'
+    ]
 
+    df = pd.read_csv('hw.csv', names=col_names, skiprows=[0])
+
+    average_height = round(numpy.mean(df.Height), 2)
+    average_weight = round(numpy.mean(df.Weight), 2)
+    return f'<p>The average height is {average_height}. The average weight is {average_weight}</p>'
 
 app.run(port=5001, debug=True)
